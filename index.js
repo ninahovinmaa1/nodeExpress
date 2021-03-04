c05fd056c070a86cf1d66dfc903c7e31cb9966bf
Nina <>
2021-03-04 11:58:17 +0100

    finished a node express app that uses routes

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-/*In this lesson you will learn about the Express framework that enables implementing and deploying
-powerful web servers based on Node. At the end of this lesson, you will be able to:
-
-Implement a web server using the Express framework
-Develop a web server that supports a REST API
-Use Express router to implement support for the REST API*/
+//Peer-graded Assignment: Assignment 1: Node Modules, Express and REST API
 
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const { runInNewContext } = require('vm');
 
-const dishRouter = require('./routes/dishRouter'); //file based node module
 const hostname = 'localhost';
 const port = 3000;
 
-// app will be using express, morgan and body-parser
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json())
 
-//Mounting a router by endpoint. Any req coming to /dishes will be handled by dishRouter
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
+
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter)
 
-//serves the static files from the public folder (__dirname = rootfolder of this app). 
-// Index.html is served on localhost:3000/
-// aboutus.html is served on localhost:3000/aboutus.html
-// none of the files is served on localhost:3000/aboutus.txt (wrong fileformat). Only the regular res.end(<html>.... this is and Express Server....</html>)
-app.use(express.static(__dirname+'/public'))
-
-// next is used to invoke middleware, optional param
 app.use((req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -5,9 +5,6 @@ const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
-//Mini-app for each router area, such as /dishes using Exporess router. each method chained, path removed. 
-//Mounted in the index.js
-
 dishRouter.route('/')
 .all((req, res, next) => {
     res.statusCode = 200;
@@ -18,7 +15,7 @@ dishRouter.route('/')
     res.end('Will send all the dishes to you!');
 })
 .post((req, res, next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details ' + req.body.description); //expects json in the res body {"name": "soup", "description": "delicious asian fish soup"}
+    res.end('Will add the dish: ' + req.body.name + ' with details ' + req.body.description);
 })
 .put((req, res, next) => {
     res.statusCode = 403;
@@ -28,23 +25,26 @@ dishRouter.route('/')
     res.end('Deleting all the dishes!');
 })
 
-.get('/dishes/:dishId', (req,res,next) => {
+
+dishRouter.route('/:dishId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
     res.end('Will send details of the dish ' + req.params.dishId + ' to you!');
 })
-
-//assumed that /dishes/23 already exists, created in app.post('/dishes, (req,res)....), soup
-.post('/dishes/:dishId', (req, res, next) => {
+.post((req, res, next) => {
     res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/' + req.params.dishId);
+    res.end('POST operation not supported on /dishes/' + req.params.dishId); 
 })
-
-.put('/dishes/:dishId', (req, res, next) => {
+.put((req, res, next) => {
     res.write('Updating the dish: ' + req.params.dishId) + '\n';
     res.end('Will update the dish: ' + req.body.name + 'with details ' + req.body.description);
 })
-
-.delete('/dishes/:dishId', (req,res,next) => {
-    res.end('Deleting dish: ' + req.parans.dishId);
+.delete((req,res,next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
 });
 
 module.exports = dishRouter;
